e15d4e2ce059c1548d93b948b285c0dc6156b0d6
sudharshanreddy <sudharshanreddy@ADFL-1130.ADFDATA.NET>
2021-05-28 23:03:37 +0530

    added sql queries

---
+++ adf/src/main/java/com/example/adf/dto/Lead.java
@@ -0,0 +1,130 @@
+package com.example.adf.dto;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+/**
+ * The persistent class for the bankaccount database table.
+ * 
+ */
+@Entity
+@Table(name = "lead")
+@NamedQuery(name = "lead.findAll", query = "SELECT b FROM Lead b")
+public class Lead implements Serializable,Cloneable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue
+  private long id;
+
+  @Column(name = "fname")
+  private String fname;
+
+   @Temporal(TemporalType.TIMESTAMP)
+  private Date datestamp;
+
+  @Column(name = "SFBankID")
+  private String lname;
+
+  @Column(name = "empType")
+  private String empType;
+  
+  @Column(name = "empStatus")
+  private Boolean empStatus;
+  @Column(name="adf_bankID")
+  private String adfBankID;
+
+
+  public Lead() {}
+
+
+public long getId() {
+	return id;
+}
+
+
+public void setId(long id) {
+	this.id = id;
+}
+
+
+public String getFname() {
+	return fname;
+}
+
+
+public void setFname(String fname) {
+	this.fname = fname;
+}
+
+
+public Date getDatestamp() {
+	return datestamp;
+}
+
+
+public void setDatestamp(Date datestamp) {
+	this.datestamp = datestamp;
+}
+
+
+public String getLname() {
+	return lname;
+}
+
+
+public void setLname(String lname) {
+	this.lname = lname;
+}
+
+
+public String getEmpType() {
+	return empType;
+}
+
+
+public void setEmpType(String empType) {
+	this.empType = empType;
+}
+
+
+public Boolean getEmpStatus() {
+	return empStatus;
+}
+
+
+public void setEmpStatus(Boolean empStatus) {
+	this.empStatus = empStatus;
+}
+
+
+public String getAdfBankID() {
+	return adfBankID;
+}
+
+
+public void setAdfBankID(String adfBankID) {
+	this.adfBankID = adfBankID;
+}
+
+
+@Override
+public String toString() {
+	return "Lead [id=" + id + ", fname=" + fname + ", datestamp=" + datestamp + ", lname=" + lname + ", empType="
+			+ empType + ", empStatus=" + empStatus + ", adfBankID=" + adfBankID + "]";
+}
+
+}
---
+++ adf/src/main/resources/schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS lead;
+  
+CREATE TABLE lead (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  fname VARCHAR(50) NOT NULL,
+  lname VARCHAR(50) NOT NULL,
+  income int DEFAULT NULL,
+  empType VARCHAR(100) NOT NULL,
+  empStatus VARCHAR(50) NOT NULL,
+  datestamp datetime DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS leadBidStatus;
+
+CREATE TABLE leadBidStatus (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  leadId long NOT NULL,
+  bidAmount int(50) NOT NULL,
+  listing_id int DEFAULT NULL,
+  bidStatus VARCHAR(50) NOT NULL,
+  datestamp datetime DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS leadModelScores;
+
+CREATE TABLE leadModelScores (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  leadId long NOT NULL,
+  narScore int DEFAULT NULL,
+  riskScore int DEFAULT NULL,
+  datestamp datetime DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS leadListingRequest;
+
+CREATE TABLE leadListingRequest (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  leadId long NOT NULL,
+  request blob DEFAULT NULL,
+  datestamp datetime DEFAULT CURRENT_TIMESTAMP
+);
